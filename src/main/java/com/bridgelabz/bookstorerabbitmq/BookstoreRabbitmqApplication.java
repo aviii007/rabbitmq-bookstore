4f6dad034c75b834abaa8e5f98b379bbd13b9bb9
aviii007 <kanojia.avi@gmail.com>
2022-12-08 17:05:57 +0530

    Added: The rabbitmq for bookstore project

---
+++ src/main/java/com/bridgelabz/bookstorerabbitmq/BookstoreRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.bookstorerabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/bookstorerabbitmq/dto/UserStatus.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.bookstorerabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserStatus {
+
+    private User user;
+    private String status;
+    private String message;
+}
+
